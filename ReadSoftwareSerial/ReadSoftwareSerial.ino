0676d6851350d4f6311c6265e8045681351eb328
Marti Kaljuve <martik@gmail.com>
2011-10-06 19:21:12 +0300

    Added simple software serial reader for pin 11.

---
+++ ReadSoftwareSerial/ReadSoftwareSerial.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial serial = SoftwareSerial(11, 3);
+
+void setup() {
+  serial.begin(57600);
+  Serial.begin(115200);
+}
+
+char c;
+void loop() {
+  if (serial.available()) {
+    c = serial.read(); 
+    if (c == '$')
+      Serial.println();
+    Serial.write(c);
+  }
+}
