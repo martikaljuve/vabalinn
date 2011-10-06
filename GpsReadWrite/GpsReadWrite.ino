77aaa110091682f348ad81a9621e9512fdee4146
Marti Kaljuve <martik@gmail.com>
2011-10-06 23:15:09 +0300

    Added methods for writing to SD.

--- GpsReadWrite/GpsReadWrite.ino
+++ GpsReadWrite/GpsReadWrite.ino
@@ -9,10 +9,13 @@ char* alt;
 
 char* sats;
 
+#define GPS_FILE_SIZE 20
+char gpsFile[GPS_FILE_SIZE] = "GPSLOG00.txt";
+
+#define DEBUG
 #define CH_CR 0x0D 
 #define CH_LF 0x0A
 const char CRLF[]={CH_CR, CH_LF, 0};
-
 
 void setup()  
 {
@@ -28,19 +31,20 @@ void setup()
   char s1[40];
   mySerial.print(NMEA_ConstructSentence(s1,"PMTK220,200")); //200 for 5 HZ 1000 for 1 Hz
   char s2[40];
-  mySerial.print(NMEA_ConstructSentence(s2,"PMTK314,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")); //For GGA messages once every 5 messages
+  mySerial.print(NMEA_ConstructSentence(s2,"PMTK314,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")); //For GGA messages once every 5 messages
   char s3[40];
   mySerial.print(NMEA_ConstructSentence(s3,"PMTK251,14400")); //Not sure why this has to be sent everytime but won't work without 
-   
-   
+
   newgps = false;
   rawgps="";
   utctime = 0;
 
-  free(s4);
   free(s1);
   free(s2);
   free(s3);
+  free(s4);
+  
+  sdSetup();
 }
 
 //
@@ -48,11 +52,14 @@ void setup()
 //
 void loop()
 {
-  if (mySerial.available()) {
+  sdOpen(gpsFile);
+  while (mySerial.available()) {
+    
+#ifdef DEBUG
     Serial.print((char)mySerial.read());
+#endif
   }
 }
-
 
 char * NMEA_ComputeChecksum(char *s) 
 { 
@@ -76,3 +83,27 @@ char * NMEA_ConstructSentence(char *s, char *msg )
   strcat(s, CRLF);
   return(s); 
 }
+
+/*
+Supported NMEA Sentences
+ 0 NMEA_SEN_GLL,     // GPGLL interval - Geographic Position - Latitude longitude  
+ 1 NMEA_SEN_RMC,     // GPRMC interval - Recommended Minimum Specific GNSS Sentence  
+ 2 NMEA_SEN_VTG,     // GPVTG interval - Course Over Ground and Ground Speed  
+ 3 NMEA_SEN_GGA,     // GPGGA interval - GPS Fix Data  
+ 4 NMEA_SEN_GSA,     // GPGSA interval - GNSS DOPS and Active Satellites  
+ 5 NMEA_SEN_GSV,     // GPGSV interval - GNSS Satellites in View  
+ 6 NMEA_SEN_GRS,     // GPGRS interval - GNSS Range Residuals  
+ 7 NMEA_SEN_GST,     // GPGST interval - GNSS Pseudorange Errors Statistics  
+ 13 NMEA_SEN_MALM,    // PMTKALM interval - GPS almanac information  
+ 14 NMEA_SEN_MEPH,    // PMTKEPH interval - GPS ephmeris information  
+ 15 NMEA_SEN_MDGP,    // PMTKDGP interval - GPS differential correction information  
+ 16 NMEA_SEN_MDBG,    // PMTKDBG interval – MTK debug information
+ 
+Supported Frequency Setting 
+ 0 - Disabled or not supported sentence  
+ 1 - Output once every one position fix  
+ 2 - Output once every two position fixes  
+ 3 - Output once every three position fixes  
+ 4 - Output once every four position fixes  
+ 5 - Output once every five position fixe
+*/
---
+++ GpsReadWrite/SD.ino
@@ -0,0 +1,44 @@
+#include <SD.h>
+
+const int chipSelect = 10; // Hardware CS pin on most Arduinos
+
+void sdSetup() {
+  pinMode(chipSelect, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+#ifdef DEBUG
+    Serial.println("Card failed, or not present");
+#endif
+    return;
+  }
+#ifdef DEBUG
+  Serial.println("card initialized.");
+#endif
+}
+
+File dataFile;
+void sdOpen(char* file) {
+  dataFile = SD.open(file, FILE_WRITE);
+}
+
+void sdClose() {
+  dataFile.close();
+}
+
+void sdWrite(String dataString) {
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+
+#ifdef DEBUG
+    Serial.println(dataString);
+#endif
+  }  
+  // if the file isn't open, pop up an error:
+  else {
+#ifdef DEBUG
+    Serial.println("error opening datalog.txt");
+#endif
+  }
+}
